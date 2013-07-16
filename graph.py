d35320be790b5eaf5c47206bf97b496d38f3e150
Sayan-Paul <sayan_666@yahoo.com>
2013-07-16 05:36:55 +0530

    Priority queue added

--- graph.py
+++ graph.py
@@ -1,5 +1,27 @@
 #Graph Implementation
 
+import heapq,collections
+
+class PriorityQueue:
+    """
+      Implements a priority queue data structure.
+    """
+    def  __init__(self):
+        self.heap = []
+        self.count = 0
+
+    def push(self, item, priority):
+        entry = (priority, self.count, item)
+        heapq.heappush(self.heap, entry)
+        self.count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
 class Graph(object):
     "Undirected Unweighted Graph container"
     def __init__(self):
